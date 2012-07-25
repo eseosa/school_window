81106881bd2d608f14610f6b1af7a69859d35c2f
Eseosa Oshodin <eseosa.oshodin.10@googlemail.com>
2012-07-25 22:03:37 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SchoolWindow::Application.config.secret_token = 'ce76b9ecdb3a288f0868617897f007a7613009bc5e909f19ec8b3c01e58a2abfcc07831286d76a98a4fe034a774df5f6dd45e4f0a026907f7be02dd8159fd830'
